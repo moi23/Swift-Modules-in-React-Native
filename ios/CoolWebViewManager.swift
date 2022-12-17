6f1ad54e2e65622bf179ae55867396084103a191
Moises Bruxo do React Native <moisesboas@gmail.com>
2022-12-17 16:43:27 -0300

    feat: its working swift brothaaa

--- ios/CoolWebViewManager.swift
+++ ios/CoolWebViewManager.swift
@@ -5,23 +5,37 @@
 //  Created by Moises Miranda Vilas Boas on 17/12/22.
 //
 
+import SwiftUI
 import Foundation
 
-@objc(CoolWebViewManager)
-class CoolWebViewManager: RCTViewManager{
-  override func view() -> UIView! {
-    let label = UILabel()
-    
-    label.text = "Hello Dude I'm native code swift and RN"
-    
-    label.textColor = .blue
-    label.textAlignment = .center
-    return label
+struct ContentView: View {
+  var body: some View {
+
+    Text("It's workinng with SWIFTUI BRO!!! ")
+  }
+}
+
+struct ContentViewWrapper: UIViewControllerRepresentable {
+  func makeUIViewController(context: UIViewControllerRepresentableContext<ContentViewWrapper>) -> UIViewController {
+    let hostingController = UIHostingController(rootView: ContentView())
+    return hostingController
   }
   
+  func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<ContentViewWrapper>) {
+    
+  }
+}
+
+@objc(CoolWebViewManager)
+class CoolWebViewManager: RCTViewManager {
+  override func view() -> UIView! {
+    let contentViewWrapper = ContentViewWrapper()
+    
+    let hostingController = UIHostingController(rootView: ContentView())
+    return hostingController.view
+  }
   
   override static func requiresMainQueueSetup() -> Bool {
     return true
   }
 }
-
